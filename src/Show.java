5426ec4f2effc1dcb910d1f9d8a001c2de21a7f8
280togu <akovlevaleksandr210@gmail.com>
2026-05-19 19:24:39 +0300

    123

---
+++ src/Show.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Show {
+    String title;
+    int duration;
+    Director director;
+    ArrayList<Actor> listOfActors;
+
+    public Show(String title, int duration, Director director, ArrayList<Actor> listOfActors) {
+        this.title = title;
+        this.duration = duration;
+        this.director = director;
+        this.listOfActors = listOfActors;
+    }
+    public void printActors() {
+        for (Actor actor : listOfActors) {
+            System.out.println(actor.getName() + " " +
+                    actor.getSurname() +
+                    " (" + actor.getHeight() + ")");
+        }
+    }
+    public void addActor(Actor actor) {
+        if (listOfActors.contains(actor)) {
+            System.out.println("Такой актёр уже есть в спектакле!");
+            return;
+        }
+        listOfActors.add(actor);
+    }
+    public void replaceActor(String surname, Actor newActor) {
+        for (int i = 0; i < listOfActors.size(); i++) {
+            if (listOfActors.get(i).getSurname().equals(surname)) {
+                listOfActors.set(i, newActor);
+                return;
+            }
+        }
+
+        System.out.println("Актёр с такой фамилией не найден!");
+    }
+}
---
+++ src/Theatre.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Theatre {
+    public static void main(String[] args) {
+
+        Actor actor1 = new Actor("Иван", "Петров", Gender.MALE, 180);
+        Actor actor2 = new Actor("Анна", "Смирнова", Gender.FEMALE, 165);
+        Actor actor3 = new Actor("Дмитрий", "Ковалёв", Gender.MALE, 175);
+
+        Director director1 = new Director("Сергей", "Пашинин", Gender.MALE, 10);
+        Director director2 = new Director("Ольга", "Иванова", Gender.FEMALE, 5);
+
+        String musicAuthor = "Игорь Стравинский";
+        String choreographer = "Михаил Фокин";
+
+        ArrayList<Actor> actorsForShow = new ArrayList<>();
+        actorsForShow.add(actor1);
+        actorsForShow.add(actor2);
+
+        Show show = new Show("Гамлет", 120, director1, new ArrayList<>(actorsForShow));
+
+        MusicalShow opera = new Opera("Кармен", 150, director2, new ArrayList<>(actorsForShow), musicAuthor, "Либретто оперы Кармен", 20);
+
+        MusicalShow ballet = new Ballet("Лебединое озеро", 140, director1, new ArrayList<>(actorsForShow), musicAuthor, "Либретто балета Лебединое озеро", choreographer);
+
+        show.addActor(actor3);
+        opera.addActor(actor3);
+
+        System.out.println("=== Гамлет ===");
+        show.printActors();
+
+        System.out.println("=== Опера ===");
+        opera.printActors();
+
+        System.out.println("=== Балет ===");
+        ballet.printActors();
+
+        Actor replacement = new Actor("Новый", "Актёр", Gender.MALE, 185);
+
+        show.replaceActor("Петров", replacement);
+
+        System.out.println("=== Гамлет после замены ===");
+        show.printActors();
+
+        ballet.replaceActor("Несуществующий", actor1);
+
+        System.out.println("=== Опера: либретто ===");
+        opera.printLibretto();
+
+        System.out.println("=== Балет: либретто ===");
+        ballet.printLibretto();
+    }
+}
